65f4ab893573e8641b25a9083c8f2570ebf88802
Karan Chaudhary <karan.chaudhary@aitc.ai>
2024-01-22 17:06:06 +0545

    modified transactions

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -3,7 +3,7 @@ import asyncHandler from "../utils/asyncHandler";
 
 // create new transaction
 const createNewTransaction = asyncHandler(async (req: Request, res: Response) => {
-    const resBody = req.body;
     const { member_id, book_id, borrow_date, return_date } = req.body;
     
+
 })
